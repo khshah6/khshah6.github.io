0bfb94ce359f25a8d8ccb7aaba37bdf5cad96a6e
Kanchi Shah <kanchishah@Kanchis-MacBook-Pro.local>
2018-10-13 12:36:13 -0500

    react-create-app on Heroku

---
+++ src/fp-react/build/precache-manifest.a79dd199496375ecf6575807e9d68ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1eac0697ecfdebaf5b7",
+    "url": "/static/js/main.d1eac069.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "d1eac0697ecfdebaf5b7",
+    "url": "/static/css/main.22410752.chunk.css"
+  },
+  {
+    "revision": "030f3144c7a1d96e1eb91e4fa83d0474",
+    "url": "/index.html"
+  }
+];
